7308d94ead79d33334d8b17c0941a0d17572bc43
TianchengLiRed <tiancl28@uci.edu>
2026-05-28 14:17:48 -0700

    Die system

--- Assets/Script/System/Health/PlayerHealth.cs
+++ Assets/Script/System/Health/PlayerHealth.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : HealthManager
 {
@@ -14,7 +15,10 @@ public class PlayerHealth : HealthManager
 
     protected override void Die()
     {
+        base.Die();
         Debug.Log("Player died. Game Over.");
+        QuestManager.Instance.SetQuest(QuestManager.QuestState.TalkToNPC);
+        SceneManager.LoadScene("Station");
         // 打开 GameOver UI
     }
     public override void TakeDamage(float damage)
@@ -23,7 +27,6 @@ public class PlayerHealth : HealthManager
         PlayerAttacked.Instance.PlayerAttackedEffect();
         CameraShake.Instance.Shake();
         EventBus.Trigger("PlayerAttacked");
-
         EventBus.Trigger("PlayerDamaged");
    }
 }
